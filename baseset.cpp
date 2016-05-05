cfce5e3bdd3ffa16eaa95cf6170b447c43a66dd6
C.J. Thompson <cjt5144@gmail.com>
2016-05-05 00:09:14 -0400

    Cleaned baseset
    Cleaned baseset interface
    Added limits
    Moved based typedef into namespace
    Renamed members for clarity
    Cleaned constructors
    Separated modifying and non-modifying accessors
    Added operators
    Commented-out implementation file

--- baseset.cpp
+++ baseset.cpp
@@ -1,44 +1,78 @@
-#include "baseset.h"
-#include <cstring>
-
-namespace ct {
-
-	// Constructors
-	
-	Baseset::Baseset(baset number, int base) {
-		dts = new char[1];
-		dts[0] = '0';
-		b = base;
-	}
-	
-	Baseset::Baseset(const char* d, const baset & l, int base) {
-		dts = new char[l];
-		b = base;
-		for(baset i = 0; i < l; i++) {
-			dts[i] = d[i];
-		}
-	}
-	
-	Baseset::Baseset(const Baseset & bs) {
-		Baseset(bs.digits(), bs.length(), bs.base());
-	}
-	
-	// Destructor
-	
-	Baseset::~Baseset() {
-		delete [] dts;
-	}
-	
-	// Accessors
-	
-	const char Baseset::at(const baset & i) const {
-		if(i < l)
-			return dts[i];
-		else
-			return 0;
-	}
-	
-	baset Baseset::decimal() const {
-		return 0;
-	}
-};
+// #include "baseset.h"
+// #include <cstring>
+// 
+// namespace ct {
+// 	
+// 	// Constructors
+// 	
+// 	Baseset::Baseset(baset number, int base) {
+// 		if(base >= MINB && base <= MAXB)
+// 			b = base;
+// 		else
+// 			b = -1;
+// 		
+// 		dts = new char[1];
+// 		dts[0] = 0;
+// 	}
+// 	
+// 	Baseset::Baseset(const char* d, int base) {
+// 		if(base >= MINB && base <= MAXB) {
+// 			b = base;
+// 			
+// 			baset a = 0;
+// 			while(d[a] != 0)
+// 				a++;
+// 		
+// 			dts = new char[a];
+// 			for(baset i = 0; i < a; i++) {
+// 				dts[i] = d[i];
+// 			}
+// 		}
+// 		else {
+// 			b = -1;
+// 		
+// 			dts = new char[1];
+// 			dts[0] = 0;
+// 		}
+// 	}
+// 	
+// 	Baseset::Baseset(const char* d, const baset & a, int base) {
+// 		if(base >= MINB && base <= MAXB) {
+// 			b = base;
+// 		
+// 			dts = new char[a];
+// 			for(baset i = 0; i < a; i++) {
+// 				dts[i] = d[i];
+// 			}
+// 		}
+// 		else {
+// 			b = -1;
+// 		
+// 			dts = new char[1];
+// 			dts[0] = 0;
+// 		}
+// 	}
+// 	
+// 	Baseset::Baseset(const Baseset & bs) {
+// 		Baseset(bs.digits(), bs.length(), bs.base());
+// 	}
+// 	
+// 	// Destructor
+// 	
+// 	Baseset::~Baseset() {
+// 		delete [] dts;
+// 	}
+// 	
+// 	// Accessors
+// 	
+// 	const char Baseset::at(const baset & i) const {
+// 		if(i < l)
+// 			return dts[i];
+// 		else
+// 			return 0;
+// 	}
+// 	
+// 	baset Baseset::decimal() const {
+// 		return 0;
+// 	}
+// };
--- baseset.h
+++ baseset.h
@@ -2,35 +2,50 @@
 #define BASESET_H_
 
 #include <string>
-
-typedef unsigned long baset;
+#include <climits>
 
 namespace ct {
+
+	typedef unsigned long baset;
+	
 	class Baseset {
 		private:
 			// Minimum and Maximum base possible base numbers
-			static const int minb = 2;
-			static const int maxb = 62;
+			static const int MINB = 2;
+			static const int MAXB = 62;
+			static const baset MAXDLIN = ULONG_MAX-4 / 2; //ULONG_MAX 4 more than string::max_size
 			
-			// Holds character representation for base b
-			char* dts;
-			int b;
-			baset l;
-		
+			// Holds character representation for base mNumberBase
+			char* mDigits;
+			int mNumberBase;
+			baset mLength;
 		public:
 			// Constructors
-			Baseset(baset number=0, int base=2);
-			Baseset(const char* d, const baset & l, int base=2);
-// 			Baseset(const std::string & s, baset & l, int base=2);
+			Baseset(baset number=0, int base=10);
+			Baseset(unsigned number); // unsigned int conversion
+			Baseset(std::string d, int base);
 			Baseset(const Baseset & bs);
 			// Destructors
 			~Baseset();
-			// Accessors
+			// Modifying Accessors
+			char & at(const baset & i);
+			void changeBase(int newBase);
+			// Const Accessors
 			const char at(const baset & i) const;
-			const char* digits() const {return dts;};
-			baset length() const {return l;};
-			int base() const {return b;};
-			baset decimal() const;
+			const char* digits() const {return mDigits;}; // Not null-terminated
+			const char* digitsNT() const; // null-terminated
+			const char* reDigits() const; // Not null-terminated reverse
+			const char* reDigitsNT() const; // null-terminated reverse
+			const baset & length() const {return mLength;};
+			int base() const {return mNumberBase;};
+			const Baseset copyToBase(int newBase);
+			// Operators
+			void operator=(const Baseset & B1);
+			bool operator==(const Baseset & B1) const;
+			bool operator>(const Baseset & B1) const;
+			bool operator<(const Baseset & B1) const;
+			bool operator>=(const Baseset & B1) const;
+			bool operator<=(const Baseset & B1) const;
 	};
 };
 
